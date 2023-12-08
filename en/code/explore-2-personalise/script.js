1d15db87ee9334f31c7dc4dc7486326dbfa9819c
tessyraspberrypi <136448689+tessyraspberrypi@users.noreply.github.com>
2023-12-08 09:24:48 +0000

    project updates

--- en/code/explore-2-personalise/script.js
+++ en/code/explore-2-personalise/script.js
@@ -12,3 +12,25 @@ function changeDisplay(id){
     inputSection.style.display = "none";
     displaySection.style.display = "flex";
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const darkModeToggle = document.getElementById("darkModeToggle");
+  
+    // Check if dark mode preference is stored in local storage
+    const isDarkMode = localStorage.getItem("darkMode") === "true";
+  
+    // Set initial dark mode state based on the stored preference
+    document.body.classList.toggle("dark-mode", isDarkMode);
+    darkModeToggle.checked = isDarkMode;
+  
+    darkModeToggle.addEventListener("change", function () {
+      const isDarkMode = darkModeToggle.checked;
+  
+      // Check if dark mode is already in the desired state
+    if (isDarkMode !== document.body.classList.contains("dark-mode")) {
+      // Update body class and store the user's preference in local storage
+      document.body.classList.toggle("dark-mode", isDarkMode);
+      localStorage.setItem("darkMode", isDarkMode.toString());
+    }
+    });
+  });
