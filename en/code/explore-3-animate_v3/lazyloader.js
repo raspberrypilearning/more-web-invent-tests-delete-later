f8bc646b77d6159da9fbdc48098da80249c95bf4
Pete Bell <peter.bell@raspberrypi.org>
2024-01-08 12:59:58 +0000

    ready

--- en/code/explore-3-animate_v3/lazyloader.js
+++ en/code/explore-3-animate_v3/lazyloader.js
@@ -3,40 +3,18 @@ window.onload = function () {
 
   const lazyimages = document.querySelectorAll(".lazy");
 
-  const io = new IntersectionObserver((entries) => {
+  const image_observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
         entry.target.src = entry.target.getAttribute("data-src");
       }
     });
+      {
+        rootMargin: "0px 0px -200px 0px"; /* WANT TO CHECK THIS IS RIGHT - WHY is rootMargin GREYED OUT? */
+      }
   });
-
   for (let i = 0; i < lazyimages.length; i++) {
     const lazyimage = lazyimages[i];
-    io.observe(lazyimage);
+    image_observer.observe(lazyimage);
   }
 };
-
-/*
-
-  const snail_animation = document.querySelectorAll("lazy");
-
-  const snail_observer = new IntersectionObserver(
-    (entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add("start-crawl");
-        } else {
-          entry.target.classList.remove("start-crawl");
-        }
-      });
-    },
-    { threshold: 0.1 }
-  );
-
-  for (let i = 0; i < snail_animation.length; i++) {
-    const elements = snail_animation[i];
-
-    snail_observer.observe(elements);
-  }
-  */
