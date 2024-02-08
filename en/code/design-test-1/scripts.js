87236eeded73552b412d435bf299744da18b32c7
m-bowley <mac@raspberrypi.org>
2024-02-08 12:08:05 +0000

    Final touches on design prototype

--- en/code/design-test-1/scripts.js
+++ en/code/design-test-1/scripts.js
@@ -1,6 +1,7 @@
 const dropdown = document.getElementById("dropdown-options");
 
 function openDropdown(){
+    console.log("clicked");
     if (dropdown.classList.contains("open")){
         dropdown.classList.remove("open");
     } else {
@@ -115,7 +116,7 @@ const slot1observer = new IntersectionObserver((entries) => {
       console.log("results"); // Show learners the Console?
       slot1.classList.add("fade-in");
     }
-});
+}, {threshold: 1});
 slot1observer.observe(slot1);
 
 const slot2observer = new IntersectionObserver((entries) => {
@@ -123,14 +124,13 @@ const slot2observer = new IntersectionObserver((entries) => {
       console.log("news"); // Show learners the Console?
       slot2.classList.add("grow-in");
     }
-});
+}, {threshold: 1});
 slot2observer.observe(slot2);
 
 const slot3observer = new IntersectionObserver((entries) => {
     if (entries[0].isIntersecting) {
       console.log("news"); // Show learners the Console?
       slot3.classList.add("rise-in");
-      slot3observer.unobserve(entries[0].target);
     }
-});
+}, {threshold: 1});
 slot3observer.observe(slot3);
